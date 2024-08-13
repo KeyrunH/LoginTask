1a9ac271938eb1bd36e0c561c7836d72ebba363c
Ciaran Hartnett <keyrun.hartnett@gmail.com>
2024-08-13 13:56:02 +0100

    Updated selector file name and added new selector
    Updated filename
    Added a selector for the eye icon in the password field

--- login/code/cypress/support/loginPageSelectors.ts
+++ login/code/cypress/support/loginPageSelectors.ts
@@ -1,7 +1,8 @@
 export const loginPageSelectors = {
     emailInput: '.py-7 > :nth-child(2) > .items-left > .justify-between > .border-grey_50',
     passwordInput: ':nth-child(1) > .gap-1 > .items-left > .justify-between > .border-grey_50',
-    loginButton: '.bg-wareBlue > .flex',
+    loginButton: '.bg-wareBlue',
     loginErrorMessage: "#err-message",
-    invalidEmailError: ".flex-row > .font-medium"
+    invalidEmailError: ".flex-row > .font-medium",
+    passwordEyeIcon: "button.ml-\\[-35px\\].mr-3.flex.items-center.justify-center"
 }

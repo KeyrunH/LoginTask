d1de9908c0e37ae0a58743ef387b34deb9e7345b
Ciaran Hartnett <keyrun.hartnett@gmail.com>
2024-08-13 12:08:23 +0100

    Selector data
    Added selector data to the selectors file

---
+++ login/code/cypress/support/loginFormSelectors.ts
@@ -0,0 +1,5 @@
+export const loginPageSelectors = {
+    emailInput: '.py-7 > :nth-child(2) > .items-left > .justify-between > .border-grey_50',
+    passwordInput: ':nth-child(1) > .gap-1 > .items-left > .justify-between > .border-grey_50',
+    loginButton: '.bg-wareBlue > .flex',
+}

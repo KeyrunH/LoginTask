c7d5f2d8d42f517a2eb807bf3244c642a946fc46
Ciaran Hartnett <keyrun.hartnett@gmail.com>
2024-08-13 21:17:47 +0100

    Updated Selectors
    Updated how some of the elements were selected

--- login/code/cypress/support/loginPageSelectors.ts
+++ login/code/cypress/support/loginPageSelectors.ts
@@ -1,8 +1,8 @@
 export const loginPageSelectors = {
-    emailInput: '.py-7 > :nth-child(2) > .items-left > .justify-between > .border-grey_50',
-    passwordInput: ':nth-child(1) > .gap-1 > .items-left > .justify-between > .border-grey_50',
-    loginButton: '.bg-wareBlue',
-    loginErrorMessage: "#err-message",
-    invalidEmailError: ".flex-row > .font-medium",
-    passwordEyeIcon: "button.ml-\\[-35px\\].mr-3.flex.items-center.justify-center"
+    emailInput: 'input[placeholder="Your email address"]',
+    passwordInput: 'input[placeholder="Enter your password"]',
+    loginButton: 'button.bg-wareBlue',
+    loginErrorMessage: '#err-message',
+    invalidEmailError: 'label.text-red_800',
+    passwordEyeIcon: 'button.ml-\\[-35px\\].mr-3.flex.items-center.justify-center'
 }

1bdb677597950e43e98a47a71b549de9669e3ae8
Ciaran Hartnett <keyrun.hartnett@gmail.com>
2024-08-13 12:36:57 +0100

    Invalid password login attempt
    Added new fixture for invalid password
    Added new test to attempt to login with an invalid password

---
+++ login/code/cypress/e2e/unableToLoginWithIncorrectPassword.cy.ts
@@ -0,0 +1,16 @@
+import { loginPageSelectors } from '../support/loginFormSelectors'
+
+describe("login page", () => {
+  beforeEach("load fixture", function () {
+    cy.fixture("loginFormData").as('data');
+    cy.visit("http://localhost:3000/")
+  })
+
+it("unable to login with unregistered user", function () {
+  cy.get(loginPageSelectors.emailInput).type(this.data.unregisteredEmail)
+  cy.get(loginPageSelectors.passwordInput).type(this.data.invalidPassword)
+  cy.get(loginPageSelectors.loginButton).click()
+  cy.get(loginPageSelectors.errorMessage).contains("Error: The email or password you entered is invalid")
+})
+
+})

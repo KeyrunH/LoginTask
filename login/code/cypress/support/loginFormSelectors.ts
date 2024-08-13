bd0b2b47d31936132554a2ec5644be1e813c94bc
Ciaran Hartnett <keyrun.hartnett@gmail.com>
2024-08-13 13:06:18 +0100

    Updated error message fixture name
    Updated the name of the login error message for clarity

--- login/code/cypress/support/loginFormSelectors.ts
+++ login/code/cypress/support/loginFormSelectors.ts
@@ -2,5 +2,6 @@ export const loginPageSelectors = {
     emailInput: '.py-7 > :nth-child(2) > .items-left > .justify-between > .border-grey_50',
     passwordInput: ':nth-child(1) > .gap-1 > .items-left > .justify-between > .border-grey_50',
     loginButton: '.bg-wareBlue > .flex',
-    errorMessage: "#err-message"
+    loginErrorMessage: "#err-message",
+    invalidEmailError: ".flex-row > .font-medium"
 }
